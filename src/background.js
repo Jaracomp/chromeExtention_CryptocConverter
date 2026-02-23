e411c7cb5924611816a7a71a575dd79d43effc5a
Jaracomp <www.gorawinog@gmail.com>
2026-02-23 17:18:47 +0100

    chore: remove background service worker and welcome page redirect
    The extension no longer needs a background script to open a welcome page on install. This simplifies the extension architecture and removes unnecessary code.

--- src/background.js
+++
@@ -1,6 +0,0 @@
-chrome.runtime.onInstalled.addListener(function(details){
-    if(details.reason == "install"){
-        console.log("Extensions install");
-        chrome.tabs.create({url: "https://itog.by/bootcamp/converter/welcome.php"});
-    }
-});
